7de8da29d614782061ca532b4e50c3faf754cedb
BarsTiger <kit.barsic@gmail.com>
2022-04-29 15:18:07 +0300

    added using daun library

---
+++ modules/__init__.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('daun/modules')
+from .download import *
+from .path import *
+from .player import *
+from .process import *
+from .screenshot import *
+from .thread import *
+from .wallpaper import *
+from .wallpaperengine import *
